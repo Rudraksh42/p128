48345e79694cb06a7010e9d10dd57c131a33173d
Rohan kumar <75016222+Rudraksh42@users.noreply.github.com>
2022-05-21 09:58:21 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,55 @@
+song = "";
+song1 = "";
+song2 = "";
+
+leftWrisY = "";
+leftWristX = "";
+
+rightWristY = "";
+rightWristX = "";
+
+
+function preload()
+{
+	song = loadSound("music.mp3");
+	song = loadSound("music2.mp3")
+}
+
+
+function setup() {
+     canvas =  createCanvas(600, 500);
+     canvas.center();
+    
+    video = createCapture(VIDEO);
+    video.hide();
+
+    posenet = ml5.poseNet(video, modelLoaded);
+    posenet.on("pose", gotPoses);
+}
+function draw() {
+    image(video, 0, 0, 600, 500);
+}
+function modelLoaded(){
+    console.log("PoseNet is Intialized");
+}
+
+function play(){
+	song.play();
+	song.setVolume(1);
+	song.rate(1);
+    
+}
+function gotPoses(results){
+    if (results.length > 0){
+        console.log(results);
+        leftX = results[0].pose.leftWrist.x;
+        leftY = results[0].pose.leftWrist.y;
+        console.log("left wrist X = " + leftX + " left wrist Y = " + leftY);
+
+        rightX = results[0].pose.rightWrist.x;
+        rightY = results[0].pose.rightWrist.y;
+        console.log("right wrist X = " + rightX + " right wrist Y = " + rightY);
+
+    }
+}
+
